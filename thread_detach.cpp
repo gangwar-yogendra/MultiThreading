9610c98f9ca630c430b5cf837fb95cd3324121b8
Yogendra Kumar <gangwar.yogendra@gmail.com>
2019-01-24 22:01:57 +0530

    Thread Join and Detach

---
+++ thread_detach.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+int _Function()
+{
+    std::cout<<"_Function\n";
+}
+
+/* Main function is the main thread for this program */
+int main(int argc, char** argv)
+{
+    /* Creating another thread in main thread */
+    std::thread myThread(_Function);
+	
+    /* We can use detach() also */
+     usleep(14);
+     myThread.detach();		/* Main thread will freely on its own, */
+							/* It will works like daemon process */
+	
+    /* We can join the same thread if thread is joinable */
+    if(myThread.joinable())	/* If condition will not insert then program will crash */
+        myThread.join();
+	
+	
+    return 0;
+}
---
+++ thread_join.cpp
@@ -0,0 +1,21 @@
+/* Join thread */
+
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+int _Function()
+{
+    std::cout<<"_Function\n";
+}
+
+/* Main function is the main thread for this program */
+int main(int argc, char** argv)
+{
+    /* Creating another thread in main thread */
+    std::thread myThread(_Function);
+	
+    myThread.join();	// main thread will to finish myThread
+	
+    return 0;
+}
