4a1ae07b861bd20bf9a87a7a76d99cf1934e2db0
gangwar-yogendra <31799734+gangwar-yogendra@users.noreply.github.com>
2020-12-25 00:04:45 +0530

    Update deadlockAvoidance.cpp

--- deadlockAvoidance.cpp
+++ deadlockAvoidance.cpp
@@ -8,7 +8,6 @@
 #include <string>
 #include <unistd.h>
 #include <mutex>
-#include <fstream>
 
 using namespace std;
 
@@ -17,11 +16,7 @@ class LogFile
     std::mutex _mu1;
     std::mutex _mu2;
     ofstream f;
-public:
-    LogFile() {
-        f.open("log.txt");
-    }
-    
+public:    
     void shared_print(string msg, int value){
         std::lock(_mu1, _mu2);
         std::lock_guard<std::mutex> lock1(_mu1, std::adopt_lock);
@@ -31,9 +26,9 @@ public:
     }
     
     void shared_print_1(string msg, int value){    
-        std::lock(_mu1, _mu2);
-        std::lock_guard<std::mutex> lock1(_mu1, std::adopt_lock);
+        std::lock(_mu1, _mu2);        
         std::lock_guard<std::mutex> lock2(_mu2, std::adopt_lock);
+        std::lock_guard<std::mutex> lock1(_mu1, std::adopt_lock);
         
         cout<< "From" << ": "<<msg << value << endl;
     }
@@ -43,7 +38,7 @@ void Function_1(LogFile &log)
 {
     for(int i=0; i>=-100; i--) {
         /* Child thread will call 'shared_print' function */
-        log.shared_print(string("Child: "), i);
+        log.shared_print(string("Child Thread: "), i);
     }
 }
 
